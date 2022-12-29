839d081b61cfb549db5ba118ddcf812ff67405e0
sanggyo <ssk7808@gmail.com>
2022-12-29 11:29:24 +0900

    #10.10 APIView : refactoring

--- categories/views.py
+++ categories/views.py
@@ -2,11 +2,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySerializer
 
 
-@api_view(["GET", "POST"])
+class Categories(APIView):
+    def get(self, request):
+        all_categories = Category.objects.all()  # 모든 카테고리 가져와서
+        serializer = CategorySerializer(all_categories, many=True)  # 시리얼라이징
+        return Response(serializer.data)
+        # 하고 전부 다 보여줌
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)  # request.data : 유저가 보낸 데이터
+        if serializer.is_valid():
+            # CategorySerializer에 적어놓은 형식이랑 유저가 보낸 데이터의 형식이 같은지 검증
+            new_category = (
+                serializer.save()
+            )  # 검증된 정보를 가지고 save (create메서드) 를 통해 새로운 Category 생성
+            return Response(CategorySerializer(new_category).data)  # 생성된 Category 보여줌
+        else:
+            return Response(serializer.errors)
+
+
+""" @api_view(["GET", "POST"])
 def categories(request):
     if request.method == "GET":
         all_categories = Category.objects.all()  # 모든 카테고리 가져와서
@@ -16,18 +36,47 @@ def categories(request):
         )
     elif request.method == "POST":
         serializer = CategorySerializer(data=request.data)  # request.data : 유저가 보낸 데이터
-        if (
-            serializer.is_valid()
-        ):  # CategorySerializer에 적어놓은 형식이랑 유저가 보낸 데이터의 형식이 같은지 검증
+        if serializer.is_valid():
+            # CategorySerializer에 적어놓은 형식이랑 유저가 보낸 데이터의 형식이 같은지 검증
             new_category = (
                 serializer.save()
             )  # 검증된 정보를 가지고 save (create메서드) 를 통해 새로운 Category 생성
             return Response(CategorySerializer(new_category).data)  # 생성된 Category 보여줌
         else:
+            return Response(serializer.errors) """
+
+
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
+        return category
+
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = CategorySerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        # 원래 있는 데이터와 유저가 있는 데이터 모두를 가지고 CategorySerializer 를 만든다.
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
             return Response(serializer.errors)
 
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
 
-@api_view(["GET", "PUT", "DELETE"])
+
+""" 
 def category(request, pk):
     # 먼저 category pk 가 있는지 확인한 후 없으면 에러 띄움
     try:
@@ -52,4 +101,4 @@ def category(request, pk):
             return Response(serializer.errors)
     elif request.method == "DELETE":
         category.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+        return Response(status=HTTP_204_NO_CONTENT) """
